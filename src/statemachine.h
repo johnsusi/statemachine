aa4bfc90ab97a6b384736524b54081f07505fe2f
John Susi <john@susi.se>
2016-07-23 21:11:25 +0200

    Initial commit

---
+++ src/statemachine.h
@@ -0,0 +1,137 @@
+#ifndef __STATEMACHINE_H
+#define __STATEMACHINE_H
+
+#include <stdexcept>
+#include <tuple>
+
+template <typename CRTP, typename... States>
+class StateMachine
+{
+public:
+
+  template <typename Event> void process_event(Event&& event = {})
+  {
+    auto& index = std::get<Index>(states);
+    find_type_by_index(index.value, states, [&](auto&& s1) {
+      Accessor::invoke_state(*this->crtp(), s1, std::forward<Event>(event));
+    });
+  }
+
+  template <typename Event, typename... Ts> void process_event(Ts... args)
+  {
+    process_event<Event>({std::forward<Ts>(args)...});
+  }
+
+  struct invoke_helper {
+    template <typename F, typename... Ts>
+    static auto invoke_if(F&& f, Ts&&... args) -> decltype( f(std::forward<Ts>(args)...) )
+    {
+      return f(std::forward<Ts>(args)...);
+    }
+
+    static auto invoke_if(...) -> void
+    {
+    }
+  };
+
+  template <typename F>
+  void apply(F&& callback)
+  {
+    find_type_by_index(std::get<Index>(states).value, states, [&](auto&& s) {
+      invoke_helper::invoke_if(std::forward<F>(callback),s);
+    });
+  }
+
+protected:
+
+  template <typename State> void set_state() noexcept
+  {
+    auto& index = std::get<Index>(states);
+    find_type_by_index(index.value, states, [&](auto&& s1) {
+      Accessor::invoke_transition(*this->crtp(), s1, std::get<State>(states));
+    });
+    index.value = index_of<State, States...>::value;
+  }
+
+  class Accessor
+  {
+  private:
+    friend class StateMachine;
+
+    template <typename... Args>
+    static auto invoke_state(CRTP& self, Args&&... args) ->
+      decltype( self.state(std::forward<Args>(args)...) )
+    {
+      return self.state(std::forward<Args>(args)...);
+    }
+
+    static void invoke_state(...) noexcept
+    {
+    }
+
+    template <typename... Args>
+    static auto invoke_transition(CRTP& self, Args&&... args) ->
+      decltype( self.transition(std::forward<Args>(args)...) )
+    {
+      return self.transition(std::forward<Args>(args)...);
+    }
+
+    static void invoke_transition(...)
+    {
+      throw std::logic_error("Invalid state transition");
+    }
+  };
+
+private:
+
+  template <typename ...> struct index_of;
+
+  template <typename T, typename... Ts>
+  struct index_of<T, T, Ts...>
+  {
+    static constexpr const std::size_t value = 0;
+  };
+
+  template <typename T, typename T1, typename... Ts>
+  struct index_of<T, T1, Ts...> : index_of<T, Ts...> {
+    static constexpr const std::size_t value = index_of<T, Ts...>::value + 1;
+  };
+
+  template <std::size_t I, std::size_t N>
+  struct find_type_by_index_helper
+  {
+    template <typename Tuple, typename F>
+    static void invoke(std::size_t index, Tuple&& values, F&& callback)
+    {
+      if (index == I) callback(std::get<I>(std::forward<Tuple>(values)));
+      else find_type_by_index_helper<I+1, N>::invoke(index, std::forward<Tuple>(values), std::forward<F>(callback));
+    }
+  };
+
+  template <std::size_t N>
+  struct find_type_by_index_helper<N, N>
+  {
+    template <typename Tuple, typename F>
+    static void invoke(std::size_t index, Tuple&& values, F callback)
+    {
+    }
+  };
+
+  template <typename Tuple, typename Fn>
+  void find_type_by_index(std::size_t index, Tuple&& values, Fn&& callback)
+  {
+    using helper = find_type_by_index_helper<0, std::tuple_size<typename std::decay<Tuple>::type>::value>;
+    helper::invoke(index, std::forward<Tuple>(values), std::forward<Fn>(callback));
+  }
+
+  struct Index {
+    std::size_t value = 0;
+  };
+
+  std::tuple<States..., Index> states;
+
+  constexpr CRTP* crtp() noexcept { return (CRTP*)(this); }
+
+};
+
+#endif
---
+++ src/unittests.cpp
@@ -0,0 +1,68 @@
+#include "statemachine.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+struct Idle {};
+struct Running {};
+struct Start { int value = 0; };
+struct Stop {};
+struct Pause {};
+struct Resume {};
+
+class TestMachine : public StateMachine<TestMachine, Idle, Running>
+{
+  int value = 0;
+
+public:
+
+  bool operator==(int other) const { return value == other; }
+
+private:
+
+  friend class Accessor;
+
+  void transition(Idle, Running) {}
+
+  void transition(Running, Idle) {}
+
+  void state(Idle, Start e)
+  {
+    if (e.value > 0) value = e.value;
+    else ++value;
+    set_state<Running>();
+  }
+
+  void state(Running, Stop)
+  {
+    set_state<Idle>();
+  }
+
+};
+
+
+TEST_CASE( "Basic tests" ) {
+
+  TestMachine m;
+  REQUIRE(m == 0);
+  m.process_event<Start>();
+  REQUIRE(m == 1);
+  m.process_event<Stop>();
+  REQUIRE(m == 1);
+  m.process_event<Start>();
+  REQUIRE(m == 2);
+  m.process_event<Stop>();
+  REQUIRE(m == 2);
+  m.process_event<Start>(10);
+  REQUIRE(m == 10);
+  m.process_event<Stop>();
+  REQUIRE(m == 10);
+
+  REQUIRE_THROWS( m.apply([](Idle) { throw 42; }) );
+  REQUIRE_THROWS( m.apply([](auto) { throw 42; }) );
+  REQUIRE_THROWS( m.apply([](auto&) { throw 42; }) );
+  REQUIRE_THROWS( m.apply([](auto&&) { throw 42; }) );
+  REQUIRE_THROWS( m.apply([](const auto&) { throw 42; }) );
+  REQUIRE_NOTHROW( m.apply([](Running) { throw 42; }) );
+
+}
